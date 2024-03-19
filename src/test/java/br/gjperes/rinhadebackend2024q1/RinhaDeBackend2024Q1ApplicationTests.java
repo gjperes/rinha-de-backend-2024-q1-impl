e3b5c196887e0e0cb8cf25d42bdb6366ad116c07
gjperes <gjperes@ufpr.br>
2024-03-19 14:11:41 -0300

    testcontainers

--- src/test/java/br/gjperes/rinhadebackend2024q1/RinhaDeBackend2024Q1ApplicationTests.java
+++ src/test/java/br/gjperes/rinhadebackend2024q1/RinhaDeBackend2024Q1ApplicationTests.java
@@ -2,9 +2,17 @@ package br.gjperes.rinhadebackend2024q1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class RinhaDeBackend2024Q1ApplicationTests {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
     @Test
     void contextLoads() {
